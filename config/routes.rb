123b3bd6e16b6822f12d03c67425fa725362e201
sfekete1 <sfekete@brandeis.edu>
2018-10-14 16:42:10 -0400

    set home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/courses'
+
+  get 'static_pages/subjects'
+
+  get 'static_pages/instructors'
+
+  root 'StaticPages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
