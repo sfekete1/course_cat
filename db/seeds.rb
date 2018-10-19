0d5fd8c878af2224ce01110425c8febc7eec826e
sfekete1 <sfekete@brandeis.edu>
2018-10-18 23:39:57 -0400

    modular seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,10 @@
 
 #create array of hashes and add the key values to my db 
 
-courseParse = JSON.parse(File.read("public/course.json")); 0
-subjectParse = JSON.parse(File.read("public/subject.json")); 0
-instructorParse = JSON.parse(File.read("public/instructor.json")); 0
-#b = JSON.parse(File.read("public/instructor.json")); 0
-#c = JSON.parse(File.read("public/subject.json")); 0
-courseParse.each {|line| line['identifier']= line.delete('id')}
-subjectParse.each {|line| line['identifier']= line.delete('id')}
-instructorParse.each {|line| line['identifier']= line.delete('id')}
-Course.import courseParse, validate:false
-Instructor.import instructorParse, validate:false
-Subject.import subjectParse, validate:false
+[[Course, "public/course.json"],[Instructor, "public/subject.json"],[Subject, "public/instructor.json"]].each do |curr|{
+    parse = JSON.parse(File.read(curr[1])); 0
+    curr[0].import curr[1], validate:false
+}
 
 
 
