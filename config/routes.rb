d33099fb273eae94886191d241b5c851d2d954b8
sfekete1 <sfekete@brandeis.edu>
2018-10-22 19:10:52 -0400

    routes unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   resources :courses
   resources :subjects
   resources :instructors
-  get 'static_pages/home'
+  #There are future implementation aspects to put in these as well as path listings (listing 5.43)
+  #get 'static_pages/home'
 
-  get 'courses#index'
+  #get 'courses#index'
 
-  get 'subjects#index'
+  #get 'subjects#index'
 
-  get 'instructors#index'
+  #get 'instructors#index'
+
+  #get  '/signup',  to: 'users#new'
 
   root 'static_pages#home'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
