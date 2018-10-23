71713fa4b417ddf1d54abaaf670f29e4f8e12582
sfekete1 <sfekete@brandeis.edu>
2018-10-23 14:11:47 -0400

    working user password/email authent&validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023170242) do
+ActiveRecord::Schema.define(version: 20181023172423) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "identifier"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20181023170242) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
